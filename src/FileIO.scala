bbae3bb22afc6f329a287feb9ce4dcec6cf05481
hfroeb <hfroeb@gmail.com>
2016-10-28 18:04:26 -0500

    third commit

--- src/FileIO.scala
+++ src/FileIO.scala
@@ -1,6 +1,7 @@
 import java.io.{File, FileOutputStream, PrintWriter}
 import scala.collection.mutable
 import scala.io.Source
+
 /**
   * Created by halleyfroeb on 10/18/16.
   * customer_id,date,credit_card,cvv,category
@@ -8,28 +9,30 @@ import scala.io.Source
   */
 object FileIO {
   def main(args: Array[String]): Unit = {
-    val purchases: mutable.MutableList[(String, String, String, String, String)] = mutable.MutableList[(String, String, String, String, String)]()
+    val purchases = mutable.MutableList[(String, String, String, String, String)]()
 
     Source
       .fromFile("purchases.csv")
       .getLines().toList
       .drop(1)
       .foreach(line => {
-        val Array(customerId, date, creditCard, cvv, category) = line.split(",").map(_.trim)
-        val purchase = (s"${customerId}", s"${date}", s"${creditCard}", s"${cvv}", s"${category}")
+        val purchase: (String, String, String, String, String) = {
+          val Array(customerId, date, creditCard, cvv, category) = line.split(",").map(_.trim)
+          (customerId, date, creditCard, cvv, category)
+        }
         purchases += purchase
       })
 
-      println("Choose a Category: furniture, alcohol, toiletries, shoes, food, jewelry")
-      val category = io.StdIn.readLine()
-    
-      val writer = new PrintWriter(new File("filtered_purchases.prn"))
-      purchases.foreach(x => {
-        if ({x._5}.equalsIgnoreCase(category)){
-          println(s"Customer: ${x._1}, Date: ${x._2}")
-          writer.write(s"Customer: ${x._1}, Date: ${x._2}\n")
-        }
-    })
+    println("Choose a Category: furniture, alcohol, toiletries, shoes, food, jewelry")
+    val category = io.StdIn.readLine()
+
+    val writer = new PrintWriter(new File("filtered_purchases.prn"))
+    purchases
+      .filter(x => x._5.equalsIgnoreCase(category))
+      .foreach(x => {
+        println(s"Customer: ${x._1}, Date: ${x._2}")
+        writer.write(s"Customer: ${x._1}, Date: ${x._2}\n")
+      })
     writer.close()
   }
 }
